f2036ab500f5d98cc509d680c188a602811ef6cd
AhmedMagdy-10 <ahmedmagdy332002@gmail>
2025-12-01 12:40:34 +0200

    add fonts and image and create the style file to manage all app  styles

--- lib/feature/presentation/screens/splash_screen.dart
+++ lib/feature/presentation/screens/splash_screen.dart
@@ -1,10 +1,28 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:shobra_store_app/feature/presentation/screens/home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AnimatedSplashScreen(
+      nextScreen: HomeScreen(),
+      duration: 3000,
+      centered: true,
+      splash: Column(
+        children: [
+          Image.asset('assets/images/shubra logo.png'),
+          Expanded(
+            child: Lottie.asset(
+              'assets/images/Animation - 1744956806670 (1).json',
+              fit: BoxFit.fill,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
